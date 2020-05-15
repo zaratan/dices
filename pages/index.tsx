9912ed6dd4d8ae212fd0e9d0cd3a97cc4d34b3da
Denis Zaratan Pasin <denis.pasin@gmail.com>
2020-05-15 17:38:55 +0900

    Titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const index = () => (
   <div>
+    <Head>
+      <title>Dices - D&D 5</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <ul>
       <li>
         <Link href="/dnd5">
