258c15ef3a69bba7d962d92e634b506fa64f1b75
Denis Zaratan Pasin <denis.pasin@gmail.com>
2020-05-17 10:13:37 +0900

    Put button :D

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,17 @@ const index = () => (
           <a>WoD</a>
         </Link>
       </li>
+      <li>
+        <a href="https://slack.com/oauth/v2/authorize?client_id=235853654149.1121819109926&scope=commands">
+          <img
+            alt="Add to Slack"
+            height="40"
+            width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+          />
+        </a>
+      </li>
     </ul>
   </div>
 );
