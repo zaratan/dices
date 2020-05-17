bd003d00809e62e7a20d1cb363581e007e2776b6
Denis Zaratan Pasin <denis.pasin@gmail.com>
2020-05-17 10:10:56 +0900

    Install Flow OK

---
+++ pages/api/slack_install.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await fetch(
+      `https://slack.com/api/oauth.v2.access?code=${req.query.code}&client_id=${process.env.SLACK_CLIENT_ID}&client_secret=${process.env.SLACK_CLIENT_SECRET}&redirect_uri=${process.env.SLACK_REDIRECT_URL}`
+    );
+
+    res.writeHead(302, {
+      Location: '/',
+    });
+    res.end();
+    return {};
+  } catch (e) {
+    // something went wrong
+    res.status(500).json({ error: e.message });
+  }
+};
